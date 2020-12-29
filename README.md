abeb6f3a5b61aad979e8b8129bdd1d49cfe742e5
jelte.derksen@rhdhv.com <jelte.derksen@rhdhv.com>
2020-12-29 20:48:23 +0100

    Minimal viable product

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # A simple YouTube Downloader
 
-Based on tkinter and pytube
+The idea was to create a simple GUI based YouTube Downloader
+
+## Dependencies
+
+* tkinter  
+* pytube  
+
+## To Do
+
+Create a better looking ensemble with classes instead of this gibberish mess  
+Add a conversion tool for converting it to MP3 instead of keeping it MP4
--- pytube_gui/run.py
+++ pytube_gui/run.py
@@ -1,9 +1,17 @@
 import os
 import tkinter as tk
 import tkinter.font as tkfont
+from tkinter.filedialog import askdirectory
 from pytube import YouTube
 
 basedir = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_video_url():
+    item = url_entry.get()
+    path = askdirectory(initialdir="/", title="Select Directory")
+    YouTube(item).streams.first().download(output_path=path)
+
 
 root = tk.Tk()
 pic = tk.PhotoImage(file=os.path.join(basedir, "icon.png"))
@@ -15,18 +23,22 @@ root.resizable(width=True, height=True)
 cv = tk.Canvas(root, height=400, width=400, bg="#263d42")
 cv.pack()
 
-bold_font = tkfont.Font(family="Helvetica", size=12, weight="bold")
-label = tk.Label(root, text="Enter the URL", width=12, bg="#263d42")
-label.config(font=bold_font)
-cv.create_window(200, 100, window=label)
+bold_font_1 = tkfont.Font(family="Helvetica", size=12, weight="bold")
+label_1 = tk.Label(root, text="Enter the URL", width=12, bg="#263d42")
+label_1.config(font=bold_font_1)
+cv.create_window(200, 100, window=label_1)
 url_entry = tk.Entry(root)
 cv.create_window(200, 140, window=url_entry)
 
+bold_font_2 = tkfont.Font(family="Helvetica", size=10, weight="bold")
+label_2 = tk.Label(root, text="Video Downloaded", width=20, bg="#263d42")
+label_2.config(font=bold_font_2)
+cv.create_window(200, 300, window=label_2)
 
-def get_video_url():
-    item = url_entry.get()
-    path = os.chdir('Enter the path where you want to save your file')
-    YouTube(item).streams.first().download(output_path=path)
+download = tk.Button(text="Download", padx=5, pady=5,
+                     fg="white", bg="DeepSkyBlue",
+                     command=get_video_url)
+cv.create_window(200, 200, window=download)
 
 
 root.mainloop()
